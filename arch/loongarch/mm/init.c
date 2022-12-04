630c272f75757054a7283825610693f976c88448
shangyatsen <maiwj6@mail2.sysu.edu.cn>
2022-12-04 23:52:34 +0800

    zfs: empty_zero_page, vm_map_base

--- arch/loongarch/mm/init.c
+++ arch/loongarch/mm/init.c
@@ -41,7 +41,7 @@
  * don't have to care about aliases on other CPUs.
  */
 unsigned long empty_zero_page, zero_page_mask;
-EXPORT_SYMBOL_GPL(empty_zero_page);
+EXPORT_SYMBOL(empty_zero_page);
 EXPORT_SYMBOL(zero_page_mask);
 
 void setup_zero_pages(void)
