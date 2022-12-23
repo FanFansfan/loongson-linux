Maxppddcsz/openEuler

LoongArch: Add kernel livepatching support
Change-Id: I3e25cbb6e0fec883eb35371f3ff28f2ee7107fe3
Signed-off-by: Jinyang He <hejinyang@loongson.cn>
Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>

arch/loongarch/include/asm/livepatch.h
@@ -1,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_LIVEPATCH_H
+#define _ASM_LIVEPATCH_H
+
+#include <asm/inst.h>
+
+static inline int klp_check_compiler_support(void)
+{
+    return 0;
+}
+
+static inline void klp_arch_set_pc(struct pt_regs *regs, unsigned long ip)
+{
+    regs->csr_era = ip;
+}
+#endif /* _ASM_LIVEPATCH_H */

FanFansfan/loongson-linux

arch/loongarch/include/asm/livepatch.h
@@ -1,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_LIVEPATCH_H
+#define _ASM_LIVEPATCH_H
+
+#include <asm/inst.h>
+
+static inline int klp_check_compiler_support(void)
+{
+	return 0;
+}
+
+static inline void klp_arch_set_pc(struct ftrace_regs *fregs, unsigned long ip)
+{
+	struct pt_regs *regs = ftrace_get_regs(fregs);
+
+	regs->csr_era = ip;
+}
+#endif /* _ASM_LIVEPATCH_H */