8fd7c6c35ffed9c40349d0a514d4fcb28654e6a7
Imre Deak <imre.deak@intel.com>
2023-11-16 15:18:31 +0200

    drm/dp_mst: Store the MST PBN divider value in fixed point format
    On UHBR links the PBN divider is a fractional number, accordingly store
    it in fixed point format. For now drm_dp_get_vc_payload_bw() always
    returns a whole number and all callers will use only the integer part of
    it which should preserve the current behavior. The next patch will fix
    drm_dp_get_vc_payload_bw() for UHBR rates returning a fractional number
    for those (also accounting for the channel coding efficiency correctly).
    
    Cc: Lyude Paul <lyude@redhat.com>
    Cc: Harry Wentland <harry.wentland@amd.com>
    Cc: Alex Deucher <alexander.deucher@amd.com>
    Cc: Wayne Lin <wayne.lin@amd.com>
    Cc: amd-gfx@lists.freedesktop.org
    Cc: dri-devel@lists.freedesktop.org
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Acked-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Acked-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    [Rebased changes in dm_helpers_construct_old_payload() on drm-intel-next]
    Signed-off-by: Imre Deak <imre.deak@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20231116131841.1588781-2-imre.deak@intel.com

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_helpers.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_helpers.c
@@ -31,6 +31,7 @@
 #include <drm/drm_probe_helper.h>
 #include <drm/amdgpu_drm.h>
 #include <drm/drm_edid.h>
+#include <drm/drm_fixed.h>
 
 #include "dm_services.h"
 #include "amdgpu.h"
@@ -210,16 +211,16 @@ void dm_helpers_dp_update_branch_info(
 {}
 
 static void dm_helpers_construct_old_payload(
-			struct drm_dp_mst_topology_mgr *mgr,
-			struct drm_dp_mst_topology_state *mst_state,
+			struct dc_link *link,
+			fixed20_12 pbn_per_slot_fp,
 			struct drm_dp_mst_atomic_payload *new_payload,
 			struct drm_dp_mst_atomic_payload *old_payload)
 {
-	struct drm_dp_mst_atomic_payload *pos;
-	int pbn_per_slot = mst_state->pbn_div;
-	u8 next_payload_vc_start = mgr->next_start_slot;
-	u8 payload_vc_start = new_payload->vc_start_slot;
-	u8 allocated_time_slots;
+	struct link_mst_stream_allocation_table current_link_table =
+									link->mst_stream_alloc_table;
+	struct link_mst_stream_allocation *dc_alloc;
+	int pbn_per_slot = dfixed_trunc(pbn_per_slot_fp);
+	int i;
 
 	*old_payload = *new_payload;
 
